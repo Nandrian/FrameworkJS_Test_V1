ff5e2d2cb57269c8b70ffff14b6c0ec6c926349e
Nandrian <ranarkai@gmail.com>
2021-08-18 23:21:09 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Header from './section1/header';
+import Section2 from './section2';
+import Section3 from './section3';
+import Section4 from './section4';
+import Section5 from './section5';
+import Section6 from './section6';
+import Section7 from './section7';
+import Section8 from './section8';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Section2/>
+      <Section3/>
+      <Section4/>
+      <Section5/>
+      <Section6/>
+      <Section7/>
+      <Section8/>
+    </div>
+  );
+}
+
+export default App;
